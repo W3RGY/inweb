c2f5e6f7ad8c7624c70dacfe07eb70740cdef3a1
Dima Osenkov <w3rgy1992@gmail.com>
2022-09-18 21:16:56 +0200

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,12 @@ const autoprefixer = require("gulp-autoprefixer");
 const cssbeautify = require("gulp-cssbeautify");
 const removeComments = require('gulp-strip-css-comments');
 const rename = require("gulp-rename");
-const sass = require("gulp-sass");
+const sass = require('gulp-sass')(require('sass'));
 const cssnano = require("gulp-cssnano");
 const rigger = require("gulp-rigger");
 const uglify = require("gulp-uglify");
 const plumber = require("gulp-plumber");
-const imagemin = require("gulp-imagemin");
+const imagemin = require('gulp-imagemin');
 const del = require("del");
 const panini = require("panini");
 const browsersync = require("browser-sync").create();
@@ -61,11 +61,11 @@ function html() {
     return src(path.src.html, { base: "src/" })
         .pipe(plumber())
         .pipe(panini({
-            root: 'src/',
-            layouts: 'src/tpl/layouts/',
-            partials: 'src/tpl/partials/',
-            helpers: 'src/tpl/helpers/',
-            data: 'src/tpl/data/'
+            root:       'src/',
+            layouts:    'src/tpl/layouts/',
+            partials:   'src/tpl/partials/',
+            helpers:    'src/tpl/helpers/',
+            data:       'src/tpl/data/'
         }))
         .pipe(dest(path.build.html))
         .pipe(browsersync.stream());
@@ -76,7 +76,6 @@ function css() {
         .pipe(plumber())
         .pipe(sass())
         .pipe(autoprefixer({
-            browsers: ['last 8 versions'],
             cascade: true
         }))
         .pipe(cssbeautify())
