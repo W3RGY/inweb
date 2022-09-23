2ff77cc8beb6a4f656bab46a57c98afef7463894
Dima Osenkov <w3rgy1992@gmail.com>
2022-09-23 18:51:18 +0200

    Delete src directory

--- src/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-//= ../../../node_modules/bootstrap/js/dist/util.js
-//= ../../../node_modules/bootstrap/js/dist/modal.js
-//= ../../../node_modules/slick-carousel/slick/slick.js
-
-$(function() {
-
-    //= components/slick.js
-
-});
